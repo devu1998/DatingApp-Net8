171ec193b4c401c0c88d12ce0b9bc68c6b782927
Devaraj <sdevaraj628@gmail.com>
2025-03-11 10:58:56 +0530

    End of Section 2

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,44 @@
+using System;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")] //api/users
+
+public class UsersController(DataContext context) : ControllerBase
+{
+    // inside controller we can create the api endpoints
+    [HttpGet]
+    // we gonna write the method which results the http response to the client.
+
+    // made GetUsers method as asynchronous
+    // best practice is to do asynchronous code.
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users = await context.Users.ToListAsync();
+
+        return users;
+
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var user = await context.Users.FindAsync(id);
+
+        if (user == null) return NotFound();
+
+        return user;
+
+        // when it comes to testing the API we have better tool like Postman available. browser is not particularly great to use testing API
+        // endpoints
+
+        // when we complete one particular section we do changes in source control i.e, "Git"
+
+    }
+
+}
