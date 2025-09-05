b4aeeff41b819412820cd4c73a8e4c266a37420e
Devaraj <sdevaraj628@gmail.com>
2025-09-06 04:25:15 +0530

    End of section 5

--- API/Program.cs
+++ API/Program.cs
@@ -10,7 +10,7 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
-.WithOrigins("http://localhost:4200", "https://localhost:4200"));
+.WithOrigins("http://localhost:4300", "https://localhost:4300"));
 
 // we need to authicatate them before authorization
 app.UseAuthentication();
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,31 +1,35 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 // Component is decorator : is used to define angular component.
 // Inject is function : used for dependency injection
 // OnInit interface from angular core module : It's life cycle hook
 // interface used to define ngOnIt method to run appcomponent 
 // when it is initialized.
-import { RouterOutlet } from '@angular/router';
+import { NavComponent } from "./nav/nav.component";
+import { AccountService } from './_services/account.service';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [NavComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 // Initializing the component 
 export class AppComponent implements OnInit {
-  http = inject(HttpClient);
-  title = 'DatingApp';
-  users: any;
+  private accountService = inject(AccountService)
 
   // we are calling ngOnInit and executing the code inside
   ngOnInit(): void {
-    this.http.get('http://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    })
+    this.setCurrentUser();
+  }
+
+  // will create a function setUser
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
   }
 }
