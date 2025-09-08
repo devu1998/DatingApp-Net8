dfffda7f1f822faa5a1f8fea50141db876dd71c4
Devaraj <sdevaraj628@gmail.com>
2025-09-08 14:31:09 +0530

    End of section 6

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,17 +3,21 @@ import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { Router, RouterLink, RouterLinkActive } from "@angular/router";
+import { ToastrService } from 'ngx-toastr';
+import { TitleCasePipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule, RouterLink, RouterLinkActive],
+  imports: [FormsModule, BsDropdownModule, RouterLink, RouterLinkActive, TitleCasePipe],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
   accountService = inject(AccountService)
   router = inject(Router);
+  private toaster = inject(ToastrService);
   model: any = {};
 
   login() {
@@ -21,7 +25,7 @@ export class NavComponent {
     next: _ => {
       this.router.navigateByUrl('/members');
     },
-    error: error => console.log(error)
+    error: error => this.toaster.error(error.error)
    })
   }
 
